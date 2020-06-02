ba99ec319c50baeafcec32306d99c9df2331f54e
vznd7676 <vznd7676@gmail.com>
2020-06-02 23:52:38 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Текст 1");
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine("Текст 2");
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine("Проба 3");
+            Console.ReadKey();
+            // проба car 
+        }
+    }
+}
